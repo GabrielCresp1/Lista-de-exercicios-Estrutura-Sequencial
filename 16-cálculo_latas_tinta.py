2398c8aefb877769065a4bb59136c83b7d7eb7e7
GabrielCresp1 <gabriel.crespi@aluno.impacta.edu.br>
2026-03-15 23:16:16 -0300

    Create 16-cálculo_latas_tinta.py

---
+++ 16-cálculo_latas_tinta.py
@@ -0,0 +1,10 @@
+import math
+
+area = float(input("Digite o tamanho da área a ser pintada (m²): "))
+
+litros = area / 3
+latas = math.ceil(litros / 18)
+preco = latas * 80
+
+print("Quantidade de latas:", latas)
+print("Preço total: R$", preco)
